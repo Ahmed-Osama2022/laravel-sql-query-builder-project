bd8ba624425b7aac8eb9aca129f49fa56ad4cec5
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2025-04-11 11:12:25 +0200

    Add the files; First commit

---
+++ resources/views/users.blade.php
@@ -0,0 +1,6 @@
+<h1>
+  Hello from users Page
+</h1>
+<h3>
+  {{ $success_message }}
+</h3>
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\UsersController;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+  return view('welcome');
+});
+
+Route::resource('users', UsersController::class);
+
+
+Route::get('/time', function () {
+  /**
+   * 1- Will use the timezone from config/app.php
+   * 'timezone' => env('TIME_ZONE', 'UTC'),
+   * 2- Add TIME_ZONE='Africa/Cairo' in /.env file
+   */
+  $now = now(); // Laravel helper
+  // dd($now);
+  // echo  $carbon = Carbon::now(); // Direct Carbon usage
+  return Carbon::now();
+});
