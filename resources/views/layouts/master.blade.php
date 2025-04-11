fcf2933bb59323039bdcc26a3e40edf3ad0c80f0
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2025-04-11 11:23:02 +0200

    Add one route; add offline bootstrap and test it

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('js/bootstrap.min.js') }}">
+  <link rel="stylesheet" href="{{ asset('js/bootstrap.bundle.min.js') }}">
+
+  <title>Query Builder | Project</title>
+</head>
+
+<body>
+
+
+
+  <h1>Hello from bootstrap</h1>
+</body>
+
+</html>
--- resources/views/users.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>
-  Hello from users Page
-</h1>
-<h3>
-  {{ $success_message }}
-</h3>
